044b669895d226a0febfca27b8d76c0b4a78a3fc
Santiago Nieto <snieto2104@gmail.com>
2016-07-28 18:45:24 -0400

    added user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
+
+
   resources :tables
   resources :clubs
   resources :users
+
+  root 'sessions#check'
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'users#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
