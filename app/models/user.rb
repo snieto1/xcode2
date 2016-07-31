6bb032f1d631bdcef7b609abec7f2051827986ec
William Castellano <wcastellano13@gmail.com>
2016-07-31 11:45:54 -0400

    Added feature to check for duplicate emails in Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
+  # Email is downcase to make it comparable
+  before_save { self.email = email.downcase}
+
+  # Makes sure a name is put in and is under 50 chars
+  validates :first_name, presence: true, length: {maximum: 25}
+
+  validates :last_name, presence: true, length: {maximum: 25}
+
+  # Makes sure email is actually an email format
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # Makes sure email is put in and in correct format
+  validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
+
   has_secure_password
 end
