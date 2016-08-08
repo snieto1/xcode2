c0ef88c3528927fd8d0030a7abdf3cd17c31a9be
Santiago Nieto <snieto2104@gmail.com>
2016-08-08 19:18:56 -0400

    reservation controller fix

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,8 +6,8 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.find_by(table_id: params[:table_id])
-    @club = @reservation.table.club
+    # @reservation = Reservation.find_by(table_id: params[:table_id])
+    # @club = @reservation.table.club
     @table = Table.find(params[:table_id])
     @reservation = Reservation.create!(table: @table, user: current_user)
     redirect_to new_reservation_reservation_bottle_path(@reservation)
