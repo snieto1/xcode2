eac11313f6bafe589e6317f3f7cf5a0a34d539c4
cubanopro <rafaelgarcia@hotmail.ca>
2016-08-09 17:20:04 -0400

    Zooming in club layout picture fixed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :table
-  has_many :reservation_bottles
+  has_many :reservation_bottles, dependent: :destroy
   has_many :bottles, through: :reservation_bottles
 end
